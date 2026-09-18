2c7b9f2513511b76a7cef97bd8285b083192b5ef
ibala-dev <balaganesh.dharmaraj07@gmail.com>
2026-09-18 07:35:20 +0530

    New card button added

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 let firstCard = 10
-let secondCard = 11
+let secondCard = 5
 let sum = firstCard + secondCard
 let hasBlackJack = false
 let isAlive = true
@@ -7,28 +7,35 @@ let message = ""
 let message_El = document.getElementById("message-el")
 let sumEl = document.getElementById("sum-el")
 let cards_El = document.getElementById("cards-el") 
+let card = 5
 
-function start_game (){
+function startGame () {
+    renderGame()
+}
+
+function renderGame (){
 sumEl.textContent = "sum: " + sum
 cards_El.textContent = "cards: " +   firstCard + " " + secondCard
 if(sum<=20){
- message =  "Do you want to draw a new card? ☹️"
+ message =  "Do you want to draw a new card?"
 }
 else if( sum == 21)
 {
-    message = "Wahoo! you have got  a blackjack 🤑"
+    message = "Wahoo! you have got  a blackjack"
     hasBlackJack = true
 
 }
 else {
-   message = "You're out of the game! 😭"
+   message = "You're out of the game!"
     isAlive = false
     console.log(isAlive)
 }
 
-
-console.log(message)
-message_el.textContent = message
+message_El.textContent = message
 }
 
+function newCard () {
+    sum += card
+    startGame()
+}
  
